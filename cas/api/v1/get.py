6c24b24b47ab4e107b3e3a64ff2c3cc0498ed96c
whn <me@whn.se>
2020-06-10 00:12:46 +0200

    Binary api

--- cas/api/v1/get.py
+++ cas/api/v1/get.py
@@ -1,6 +1,6 @@
 """"""
 import base64
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body
 from fastapi.responses import Response
 
 import cas.deps as deps
@@ -8,9 +8,9 @@ import cas.deps as deps
 router = APIRouter()
 
 
-@router.get("/get")
-async def store(p: str, service=Depends(deps.service)) -> bytes:
-    res = await service.get(base64.b64decode(p))
+@router.post("/get")
+async def store(body: bytes = Body("Keys"), service=Depends(deps.service)) -> bytes:
+    res = await service.get(body)
     if res is None:
         return Response(status_code=404)
     return Response(content=res)
--- cas/api/v1/store.py
+++ cas/api/v1/store.py
@@ -1,5 +1,4 @@
 """"""
-import base64
 from typing import Any, Dict
 
 from fastapi import APIRouter, Depends
@@ -10,11 +9,7 @@ import cas.deps as deps
 router = APIRouter()
 
 
-def pointer_as_str(pointer: bytes) -> bytes:
-    return base64.b64encode(pointer)
-
-
 @router.post("/store")
 async def store(body: Dict[str, Any], service=Depends(deps.service)) -> bytes:
     res = await service.insert(body)
-    return Response(content=pointer_as_str(res))
+    return Response(content=res)
